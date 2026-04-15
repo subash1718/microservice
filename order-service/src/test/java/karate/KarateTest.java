4786f2c533c05dc64d03ed4b4d7116249ec03214
Manjunadh Reddy <manjunadhreddy@example.com>
2026-04-15 14:10:40 +0100

    Fixed Karate test and build success

--- order-service/src/test/java/karate/KarateTest.java
+++ order-service/src/test/java/karate/KarateTest.java
@@ -1,22 +1,22 @@
 package karate;
 
 import com.intuit.karate.junit5.Karate;
-import com.microservice.order_service.OrderServiceApplication; // 🔥 IMPORTANT
+import com.microservice.order_service.OrderServiceApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(
-        classes = OrderServiceApplication.class,   // 🔥 FIX
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = OrderServiceApplication.class
 )
-public class KarateTest {
+class KarateTest {
 
     @LocalServerPort
     int port;
 
     @Karate.Test
     Karate testAll() {
-        System.setProperty("karate.server.port", String.valueOf(port));
-        return Karate.run("classpath:karate").relativeTo(getClass());
+        return Karate.run("classpath:karate")
+                .systemProperty("karate.port", String.valueOf(port));
     }
 }
--- order-service/src/test/resources/karate/order.feature
+++ order-service/src/test/resources/karate/order.feature
@@ -1,16 +1,16 @@
 Feature: Order API Test
 
 Background:
-    * def port = karate.properties['port']
+    * def port = karate.properties['karate.port']
     * url 'http://localhost:' + port
 
 Scenario: Create and get orders
 
-    Given path '/orders'
-    And request { "productName": "Test", "quantity": 1 }
+    Given path 'orders'
+    And request { productName: 'Phone', quantity: 2, price: 1000, status: 'CREATED' }
     When method post
     Then status 200
 
-    Given path '/orders'
+    Given path 'orders'
     When method get
     Then status 200
